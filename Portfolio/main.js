6c06440a5c0ac89a5c9adef2ea853c58446143f5
zsigadaniel <danielzsiga@gmail.com>
2020-05-01 13:39:55 +0300

    commit

---
+++ Portfolio/main.js
@@ -0,0 +1,26 @@
+const button = document.querySelectorAll('.button h3');
+const darkTheme = document.querySelector('.darkTheme');
+const body = document.querySelector('body');
+const introduction = document.querySelector('.introduction');
+const links = document.querySelectorAll('.dark-text');
+const mainText = document.querySelectorAll('h1');
+
+darkTheme.addEventListener('click', () => {
+    links.forEach(link => {
+        link.classList.toggle('dark-text');
+        link.classList.toggle('light-text');
+    })
+    mainText.forEach(mainT => {
+        mainT.classList.toggle('light-text');
+    })
+    body.classList.toggle('dark-body');
+    darkTheme.classList.toggle('dark');
+    introduction.classList.toggle('darkLines');
+    introduction.classList.toggle('introduction');
+    if (darkTheme.innerHTML == 'Dark Theme') {
+        darkTheme.innerHTML = 'Light Theme'
+    } else if (darkTheme.innerHTML == 'Light Theme') {
+        darkTheme.innerHTML = 'Dark Theme'
+    }
+
+});
